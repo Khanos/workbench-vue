8ac622b3970989442ddf9facc4a26b2b6e0ffa4f
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2024-02-01 07:16:25 -0400

    adding composables

---
+++ vue-latest/src/utils/useFetch.js
@@ -0,0 +1,42 @@
+import { ref } from 'vue';
+const defaultOptions = {
+  cache: false,
+  simulateDelay: false,
+  simulateError: false,
+};
+export const useFetch = (url, options = defaultOptions) => {
+  const { cache, simulateDelay, simulateError } = options; 
+  const data = ref(null);
+  const error = ref(null);
+  const loading = ref(true);
+  const key = url.replace(/[^a-zA-Z0-9]/g, '');
+  const fetchCall = async () => {
+    setTimeout(async () => {
+      try {
+        if (simulateError) {
+          throw new Error('Simulated error');
+        }
+        const response = await fetch(url);
+        const result = await response.json();
+        data.value = result;
+        if (cache) {
+          localStorage.setItem(key, JSON.stringify(result));
+        }
+      } catch (err) {
+        error.value = err;
+        console.error('Error fetching data', error);
+      } finally {
+        loading.value = false;
+      }
+    }, simulateDelay ? 2000 : 0);
+  }
+  const cachedData = localStorage.getItem(key);
+  if (cache && cachedData) {
+    data.value = JSON.parse(cachedData);
+    loading.value = false;
+  } else {
+    fetchCall();
+  }
+  !cache && fetchCall();
+  return { data, error, loading, fetchCall};
+}
--- vue-latest/src/views/UsersView.vue
+++ vue-latest/src/views/UsersView.vue
@@ -1,33 +1,26 @@
 <template>
   <main class="users-container">
-    <h1>Hello from users</h1>
-    <ul class="users">
+    <h1>Hello from users {{ loading }}</h1>
+    <p v-if="loading">Loading users...</p>
+    <ul v-else class="users">
       <li v-for="user in userList" :key="user.id" class="user">
         {{ user.name }}
       </li>
     </ul>
+    <p v-if="error">Ups something bad happens ⚠</p>
   </main>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
+import { useFetch } from '../utils/useFetch';
 
-const userList = ref([]);
-
-const fetchUsers = async () => {
-  try {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users');
-    const data = await response.json();
-    console.log(data)
-    userList.value = data;
-  } catch (error) {
-    console.log('Error fetching the users', error);
-  }
-};
-
-onMounted(() => {
-  return userList.value.length > 0 ? false : fetchUsers();
+const { data: userList, error, loading } = useFetch('https://jsonplaceholder.typicode.com/users', {
+  simulateDelay: false,
+  simulateError: false,
+  cache: true,
 });
+
+console.log('userList', userList.value);
 
 </script>
 
